b5d72394092c95e359490c574ddd00dce39941bd
yichengjie <626659321@qq.com>
2019-10-18 22:31:19 +0800

    s

--- container-deep-study/src/main/java/com/yicj/container/s1/CollectionDataGeneration.java
+++ container-deep-study/src/main/java/com/yicj/container/s1/CollectionDataGeneration.java
@@ -4,5 +4,6 @@ public class CollectionDataGeneration {
 
     public static void main(String[] args) {
 
+
     }
 }
--- container-deep-study/src/main/java/com/yicj/container/s1/MapDataTest.java
+++ container-deep-study/src/main/java/com/yicj/container/s1/MapDataTest.java
@@ -1,23 +1,47 @@
 package com.yicj.container.s1;
 
+import com.yicj.container.common.MapData;
 import com.yicj.container.common.Pair;
 import com.yicj.generic.common.Generator;
 
 import java.util.Iterator;
 
-class Letters implements Generator<Pair<Integer,String>> ,Iterable<Integer>{
+class Letters implements
+        Generator<Pair<Integer,String>> ,Iterable<Integer>{
+    private int size = 9 ;
+    private int number = 1 ;
+    private char letter = 'A' ;
 
     @Override
     public Pair<Integer, String> next() {
-        return null;
+        return new Pair<Integer, String>(number++,"" +letter++);
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>(){
+            @Override
+            public boolean hasNext() {
+                return number < size;
+            }
+            @Override
+            public Integer next() {
+                return number++;
+            }
+            public void remove(){
+                throw new UnsupportedOperationException() ;
+            }
+        } ;
     }
 }
 
 public class MapDataTest {
 
+    public static void main(String[] args) {
+        //Pair Generator
+        System.out.println(MapData.map(new Letters(),11));
+        System.out.println("====================");
+        System.out.println(MapData.map(new Letters(),
+                "Pop"));
+    }
 }
