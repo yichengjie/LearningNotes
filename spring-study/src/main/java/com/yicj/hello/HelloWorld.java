a11ab4e0a11c511a42e55285ff1ac9a532822db6
yichengjie <626659321@qq.com>
2019-11-03 22:05:44 +0800

    s

--- spring-study/src/main/java/com/yicj/hello/HelloWorld.java
+++ spring-study/src/main/java/com/yicj/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 
-public class Helloworld {
+public class HelloWorld {
     public static void main(String[] args) {
         ClassPathResource res = new ClassPathResource("beans.xml") ;
         DefaultListableBeanFactory factory = new DefaultListableBeanFactory() ;
