7aa884904c294f9e37b7c00b2ac9b97d8b3b9871
yichengjie <626659321@qq.com>
2019-11-06 20:58:26 +0800

    replace方法使用

--- thread-study/src/main/java/com/yijcj/thread/pool/ThreadPoolExecutorTest.java
+++ thread-study/src/main/java/com/yijcj/thread/pool/ThreadPoolExecutorTest.java
@@ -41,7 +41,7 @@ public class ThreadPoolExecutorTest {
         @Override
         public Thread newThread(Runnable r) {
             Thread t = new Thread(r,"my-thread-" + mThreadNum.getAndIncrement()) ;
-            System.out.println(t.getName() + " has entity created");
+            System.out.println(t.getName() + " has busi created");
             return t;
         }
     }
