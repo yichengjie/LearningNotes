31e4b44131da6c6ad96b703e43003a4bb4210ae6
yichengjie <626659321@qq.com>
2019-11-11 09:48:46 +0800

    更新版本号

--- spring-aop-study/src/main/java/com/yicj/cglib/Test.java
+++ spring-aop-study/src/main/java/com/yicj/cglib/Test.java
@@ -6,7 +6,7 @@ import org.springframework.cglib.proxy.Enhancer;
 public class Test {
     public static void main(String[] args) {
         //代理类class文件存入本地磁盘
-        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\code");
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\code\\gen");
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(PersonService.class);
         enhancer.setCallback(new CglibProxyInterceptor());
