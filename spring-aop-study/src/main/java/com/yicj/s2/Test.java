9dad7ae2c3d2234e7b15bce177e394fa61cd006f
易成杰 <626659321@qq.com>
2019-11-19 17:16:56 +0800

    补充自动扫描

--- spring-aop-study/src/main/java/com/yicj/s2/Test.java
+++ spring-aop-study/src/main/java/com/yicj/s2/Test.java
@@ -6,8 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
+        String path = "com/yicj/s2/beans.xml" ;
+        //String path = "com/yicj/s2/beans2.xml" ;
+        //String path = "com/yicj/s2/beans3.xml" ;
         ApplicationContext context =
-                new ClassPathXmlApplicationContext("com/yicj/s2/beans.xml") ;
+                new ClassPathXmlApplicationContext("") ;
         ITask task = (ITask)context.getBean("taskProxy") ;
         task.execute(null);
         System.out.println(task.getClass().getName());
