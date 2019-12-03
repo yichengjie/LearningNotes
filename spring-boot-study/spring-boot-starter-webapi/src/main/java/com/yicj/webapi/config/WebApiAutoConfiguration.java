e1fbfed620339af90b2aad35ab1517ad4259009c
易成杰 <626659321@qq.com>
2019-12-03 10:09:44 +0800

    swagger整合springboot

--- spring-boot-study/spring-boot-starter-webapi/src/main/java/com/yicj/webapi/config/WebApiAutoConfiguration.java
+++ spring-boot-study/spring-boot-starter-webapi/src/main/java/com/yicj/webapi/config/WebApiAutoConfiguration.java
@@ -8,15 +8,28 @@ import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguratio
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+//https://www.jianshu.com/p/4539e312ce87
 @Configuration
 @EnableSwagger2
 //@ComponentScan("com.yicj.springboot.webapi.errors")
@@ -26,28 +39,53 @@ public class WebApiAutoConfiguration extends WebMvcConfigurationSupport {
     private TypeResolver typeResolver ;
     @Value("${springfox.api.group:[your api group name]}")
     private String apiGroupName ;
+    @Value("${springfox.api.title:[set a api title via 'springfox.api.title']}")
     private String title ;
-    private String desc ;
+    @Value("${springfox.api.description}:[add your api description via 'springfox.api.description']")
+    private String description ;
+    @Value("${springfox.api.version}:[set special api version via 'springfox.api.version']")
     private String version ;
+    @Value("${springfox.api.termsOfServiceUrl}:[set termsOfServiceUrl via 'springfox.api.termsOfServiceUrl']")
     private String termsOfServiceUrl ;
-    private String contact ;
+    @Value("${springfox.api.contactName}:[set termsOfServiceUrl via 'springfox.api.termsOfServiceUrl']")
+    private String contactName ;
+    @Value("${springfox.api.contactEmail}:[set termsOfServiceUrl via 'springfox.api.termsOfServiceUrl']")
+    private String contactEmail ;
+    @Value("${springfox.api.contactUrl}:[set termsOfServiceUrl via 'springfox.api.termsOfServiceUrl']")
+    private String contactUrl ;
+    @Value("${springfox.api.license}:Your WebAPI License")
     private String license ;
+    @Value("${springfox.api.licenseUrl}:http://yicj.com")
     private String licenseUrl ;
 
     @Bean
     public Docket api(){
+        List<ResponseMessage> responseMessageList = new ArrayList<>();
+        responseMessageList.add(new ResponseMessageBuilder().code(500)
+                .message("服务器内部错误").responseModel(new ModelRef("Error")).build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName(apiGroupName)
-                .apiInfo(new ApiInfo(title,desc,version,termsOfServiceUrl,contact,license,licenseUrl))
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .build()
                 .pathMapping("/")
                 .directModelSubstitute(Date.class,String.class)
                 .genericModelSubstitutes(ResponseEntity.class)
-                //.alternateTypeRules()
-                ;
+                //自定义规则，如果遇到DeferredResult，则把泛型类转成json
+                .alternateTypeRules(newRule(typeResolver.resolve(DeferredResult.class,
+                    typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                    typeResolver.resolve(WildcardType.class)))
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, responseMessageList)
+                .forCodeGeneration(true);
     }
 
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact(contactName, contactUrl, contactEmail);
+        return new ApiInfoBuilder().title(title).description(description).version(version)
+                .termsOfServiceUrl(termsOfServiceUrl).contact(contact).license(license)
+                .licenseUrl(licenseUrl).build();
+    }
 
 }
